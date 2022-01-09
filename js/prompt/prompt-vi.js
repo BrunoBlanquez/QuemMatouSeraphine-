f0ed1aca9ffe94c738e33752760215828355e38c
Bruno <b.blanquez@yahoo.com.br>
2022-01-09 17:08:38 -0300

    Ajustes JS paginas historia

---
+++ js/prompt/prompt-vi.js
@@ -0,0 +1,12 @@
+function escolha() { 
+  let opcao = prompt('Digite 1 ou 2 para a escolha desejada');
+  if (opcao != null) {
+    location = "../páginas-vi/" + paginaatual.dataset.paginaatual + '.' + opcao + ".html"
+  } else {
+    alert("Resposta inválida! Por favor, responda 1 ou 2")
+  }
+}
+let iniciar = document.querySelector("button")
+const paginaatual = document.getElementById("dataset");
+console.log(paginaatual.dataset.paginaatual)
+iniciar.onclick = escolha;
