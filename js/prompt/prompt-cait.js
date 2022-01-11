3fa2ddd8c086763f8bd6dc60f3240784fe9fcdbb
Bruno <b.blanquez@yahoo.com.br>
2022-01-11 19:30:22 -0300

    correção do prompt

--- js/prompt/prompt-cait.js
+++ js/prompt/prompt-cait.js
@@ -1,6 +1,6 @@
 function escolha() { 
   let opcao = prompt('Digite 1 ou 2 para a escolha desejada');
-  if (opcao != null) {
+  if (opcao != null && opcao == 1 || opcao == 2) {
     location = "../páginas-cait/" + paginaatual.dataset.paginaatual + '.' + opcao + ".html"
   } else {
     alert("Resposta inválida! Por favor, responda 1 ou 2")
